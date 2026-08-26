0d49724771d3132c34480177f86c7e2069f1e19f
roshinibyraj <roshinibyraj@users.noreply.github.com>
2026-08-26 20:51:20 +1000

    Fix crash: correct import of PolymarketTrader (default export, not named)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const { MomentumLagEngine } = require('./engine');
-const { PolymarketTrader } = require('./polymarket-trader');
+const PolymarketTrader = require('./polymarket-trader');
 
 const app = express();
 const server = http.createServer(app);
